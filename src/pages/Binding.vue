6aba93e356e07e997f1dec9f69aaf13feb59bae2
aannoni <annonialexandra@gmail.com>
2026-08-13 14:21:38 +1000

    tag attribute binding boolean and enum

--- src/pages/Binding.vue
+++ src/pages/Binding.vue
@@ -16,11 +16,32 @@
     <h3>an elment plus button who should be disabled when cookies are sold out</h3>
     <el-button v-bind:disabled="cookiesSoldOut">Buy a cookie</el-button>
   </div>
+
+  <div class="tagStyling">
+    <!-- bind boolean attribute to tag -->
+    <el-tag v-bind:closable="showClose">Favourite</el-tag>
+    <el-tag v-bind:hit="highlightBorder">Favourite</el-tag>
+    <el-tag v-bind:round="isRound">Favourite</el-tag>
+
+    <!-- bind enum attribute to tag -->
+    <el-tag v-bind:type="isSuccess">Successful</el-tag>
+    <el-tag v-bind:size="isLarge">Successful</el-tag>
+    <el-tag v-bind:effect="isDark">Successful</el-tag>
+  </div>
 </template>
 
 <script setup>
   const url = "https://www.vuemastery.com/dashboard"
   const cookiesSoldOut = true
+
+  // can bind boolean attribute to tag
+  const showClose = true
+  const highlightBorder = true
+  const isRound = true
+
+  const isSuccess = 'success'
+  const isLarge = 'large'
+  const isDark = 'dark'
 
 </script>
 
@@ -28,5 +49,9 @@
   .spacing {
     margin-bottom: 80px;
   }
+
+ .tagStyling {
+    display: flex;
+ } 
   
 </style>
