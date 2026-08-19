be539459289bde4713cdc44acdf1abf596019279
aannoni <annonialexandra@gmail.com>
2026-08-19 12:41:51 +1000

    not understanding wip events and functions

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -3,13 +3,35 @@
   <h4>Events is what is happening in the browser (click, press, submit, hover)</h4>
   <h4>then vue says 'do this' which is called an action</h4>
 
-  <el-button v-on:click="sayHello('Alex')">Press to say hello</el-button>
+  <el-button @click="sayHi">Press me to say hi</el-button>
+  <el-button @click="console.log(addNumbers(2,3))">Add numbers</el-button>
+  <el-button @click="console.log(subtractNumbers(5,4))">Subtract numbers</el-button>
+
+  <el-input v-model="userNumber"></el-input>
+  <el-button @click="console.log(doubleIt(userNumber))">Press to double input</el-button>
+
 </template>
 
 <script setup>
-  const sayHello = (name) => {
-    console.log('Hello' + name)
+  import { ref } from 'vue'
+
+  const sayHi = () => {
+    console.log('Hi')
   }
+
+  const addNumbers = (a,b) => {
+    return(a+b)
+  }
+
+  const subtractNumbers = (a,b) => {
+    return(a-b)
+  }
+
+  const userNumber = ref(0)
+  const doubleIt = () => {
+    return (userNumber * 2)
+  }
+
 </script>
 
 <style scoped>
